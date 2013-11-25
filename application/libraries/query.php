d9bcb5e4fcb216353e1c18ce013962be3bec9113
elymentree <elizarflores@yahoo.com>
2013-11-25 15:00:14 -0500

    updates

--- application/libraries/query.php
+++ application/libraries/query.php
@@ -54,9 +54,9 @@
 				 
 */
 #### INITIAL DATABASE CONFIGURATION ###########################################################################################################
- defined('DB_SERVER') ? null : define("DB_SERVER", "aryanasolutions.db.11271867.hostedresource.com");
- defined('DB_USER')   ? null : define("DB_USER"  , "aryanasolutions");
- defined('DB_PASS')   ? null : define("DB_PASS"  , "Aryana2013!");
+ defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
+ defined('DB_USER')   ? null : define("DB_USER"  , "root");
+ defined('DB_PASS')   ? null : define("DB_PASS"  , "");
  defined('DB_NAME')   ? null : define("DB_NAME"  , "aryanasolutions");
 ###############################################################################################################################################
 
